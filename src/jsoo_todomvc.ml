e7c39e1679ce3714bc8a43f63189a39b6c1edc1f
Bikal Gurung <gbikal@gmail.com>
2020-04-30 16:35:58 +0100

    prints hello world

--- src/jsoo_todomvc.ml
+++ src/jsoo_todomvc.ml
@@ -1,1 +1,8 @@
-let () = print_endline "Hello, World!"
+open Js_of_ocaml_tyxml
+open Js_of_ocaml
+module T = Tyxml_js
+
+let () =
+  let html = T.Html.(div [txt "Hello, world!"]) in
+  let app = Dom_html.getElementById "app" in
+  Dom.appendChild app (Tyxml_js.To_dom.of_div html)
