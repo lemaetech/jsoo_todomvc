ff55b37aa1a3e6c6ff06ae65e2382ff0333b2979
Bikal Gurung <gbikal@gmail.com>
2020-04-30 16:55:30 +0100

    simplify opens

--- src/jsoo_todomvc.ml
+++ src/jsoo_todomvc.ml
@@ -1,8 +1,7 @@
-open Js_of_ocaml_tyxml
+open Js_of_ocaml_tyxml.Tyxml_js
 open Js_of_ocaml
-module T = Tyxml_js
 
 let () =
-  let html = T.Html.(div [txt "Hello, world!"]) in
+  let html = Html.(div [txt "Hello, world!"]) in
   let app = Dom_html.getElementById "app" in
-  Dom.appendChild app (Tyxml_js.To_dom.of_div html)
+  Dom.appendChild app (To_dom.of_div html)
