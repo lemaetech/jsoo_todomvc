8bdd38d34454025d3bbdfbcf0867e09c1d77f158
Bikal Gurung <gbikal@gmail.com>
2020-05-01 18:47:35 +0100

    add footer after DOM loaded

--- src/jsoo_todomvc.ml
+++ src/jsoo_todomvc.ml
@@ -1,4 +1,6 @@
 open Js_of_ocaml_tyxml.Tyxml_js
+open Js_of_ocaml_lwt
+open Lwt.Infix
 open Js_of_ocaml
 
 let todo_app = Html5.(section ~a:[a_class ["todoapp"]] [])
@@ -14,11 +16,16 @@ let info_footer =
       ; p [txt "Part of "; a ~a:[a_href "http://todomvc.com"] [txt "TodoMVC"]]
       ])
 
-let () =
+let main () =
+  (* Firebug.console##log info_footer ; *)
+
   (* let now = new%js Js.date_now in *)
   (* let date = now##toString in *)
   (* let s = Js.to_string date ^ " : Hello world!" in *)
   (* let html = Html5.(div [txt s]) in *)
   let app = Dom_html.getElementById "app" in
   Dom.appendChild app (To_dom.of_section todo_app) ;
-  Dom.appendChild app (To_dom.of_footer info_footer)
+  Dom.appendChild app (To_dom.of_footer info_footer) ;
+  Lwt.return_unit
+
+let _ = Lwt_js_events.domContentLoaded () >>= main
