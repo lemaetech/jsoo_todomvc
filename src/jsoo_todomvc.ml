6dff27ebd198b53a9924f8d83902ee0d9d7f01f8
Bikal Gurung <gbikal@gmail.com>
2020-05-01 13:00:30 +0100

    print current date time

--- src/jsoo_todomvc.ml
+++ src/jsoo_todomvc.ml
@@ -2,6 +2,9 @@ open Js_of_ocaml_tyxml.Tyxml_js
 open Js_of_ocaml
 
 let () =
-  let html = Html.(div [txt "Hello, world!"]) in
+  let now = new%js Js.date_now in
+  let date = now##toString in
+  let s = Js.to_string date ^ " : Hello world!" in
+  let html = Html.(div [txt s]) in
   let app = Dom_html.getElementById "app" in
   Dom.appendChild app (To_dom.of_div html)
