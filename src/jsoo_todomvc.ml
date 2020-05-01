110399ca059724e898d6bdabfea14a6639786506
Bikal Gurung <gbikal@gmail.com>
2020-05-01 18:30:23 +0100

    add info section

--- src/jsoo_todomvc.ml
+++ src/jsoo_todomvc.ml
@@ -1,10 +1,24 @@
 open Js_of_ocaml_tyxml.Tyxml_js
 open Js_of_ocaml
 
+let todo_app = Html5.(section ~a:[a_class ["todoapp"]] [])
+
+let info_footer =
+  Html5.(
+    footer
+      ~a:[a_class ["info"]]
+      [ p [txt "Double click to edit a todo"]
+      ; p
+          [ txt "Written by "
+          ; a ~a:[a_href "http://github.com/bikallem/"] [txt "Bikal Lem"] ]
+      ; p [txt "Part of "; a ~a:[a_href "http://todomvc.com"] [txt "TodoMVC"]]
+      ])
+
 let () =
-  let now = new%js Js.date_now in
-  let date = now##toString in
-  let s = Js.to_string date ^ " : Hello world!" in
-  let html = Html.(div [txt s]) in
+  (* let now = new%js Js.date_now in *)
+  (* let date = now##toString in *)
+  (* let s = Js.to_string date ^ " : Hello world!" in *)
+  (* let html = Html5.(div [txt s]) in *)
   let app = Dom_html.getElementById "app" in
-  Dom.appendChild app (To_dom.of_div html)
+  Dom.appendChild app (To_dom.of_section todo_app) ;
+  Dom.appendChild app (To_dom.of_footer info_footer)
